5e9037279a31f6d389852b0eb324cbdc3c9e5611
Swartkind <105363742+SwartKind@users.noreply.github.com>
2023-02-17 13:00:42 +0200

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,14 +8,14 @@ int main(void)
 {
 char abc;
 
-for (abc = a; abc <= z; abc++)
+for (abc = 'a'; abc <= 'z'; abc++)
 {
 putchar(abc);
 }
-for (abc = A; abc <= Z; abc++)
+for (abc = 'A'; abc <= 'Z'; abc++)
 {
 putchar(abc);
 }
-putchar(n);
+putchar('\n');
 return (0);
 }
