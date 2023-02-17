5e549be1434675371d002a4a2d020eb2db01a876
Swartkind <105363742+SwartKind@users.noreply.github.com>
2023-02-17 19:43:38 +0200

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,22 +11,22 @@ int nums;
 int nums1;
 int nums2;
 
-for (nums = 0; nums <= 7; nums++)
+for (nums = '0'; nums <= '7'; nums++)
 {
-for (nums1 = nums + 1; nums1 <= 9; nums1++)
+for (nums1 = nums + 1; nums1 <= '9'; nums1++)
 {
-for (nums2 = nums1 + 1; nums2 <= 9; nums2++)
+for (nums2 = nums1 + 1; nums2 <= '9'; nums2++)
 {
 putchar(nums);
 putchar(nums1);
 putchar(nums2);
-if (nums == 7 && nums1 == 8 && nums2 == 9)
+if (nums == '7' && nums1 == '8' && nums2 == '9')
 break;
-putchar(,);
-putchar( );
+putchar(',');
+putchar(' ');
 }
 }
 }
-putchar(n);
+putchar('\n');
 return (0);
 }
