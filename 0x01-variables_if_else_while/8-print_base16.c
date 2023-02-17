e8d9f304238eed54bcf3aacd52c1886fba782b7a
Swartkind <105363742+SwartKind@users.noreply.github.com>
2023-02-17 13:15:47 +0200

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,14 +9,14 @@ int main(void)
 int nums;
 int nums1;
 
-for (nums = 0; nums <= 9; nums++)
+for (nums = '0'; nums <= '9'; nums++)
 {
 putchar(nums);
 }
-for (nums1 = a; nums1 <= f; nums1++)
+for (nums1 = 'a'; nums1 <= 'f'; nums1++)
 {
 putchar(nums1);
 }
-putchar(n);
+putchar('\n');
 return (0);
 }
