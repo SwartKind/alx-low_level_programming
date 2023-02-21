6699e3e256201d83c2b67f70d09e13c963a8e6db
Swartkind <105363742+SwartKind@users.noreply.github.com>
2023-02-21 08:31:39 +0200

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_sign - check input if its positive, negative or zero
@@ -11,17 +11,17 @@ int print_sign(int n)
 int i;
 if (n > 0)
 {
-_putchar(+);
+_putchar('+');
 i = 1;
 }
 else if (n < 0)
 {
-_putchar(-);
+_putchar('-');
 i = -1;
 }
 else
 {
-_putchar(0);
+_putchar('0');
 i = 0;
 }
 return (i);
