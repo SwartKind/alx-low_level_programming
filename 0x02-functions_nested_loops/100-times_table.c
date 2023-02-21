ec3c7c78989cedd608f673afd2fc787bc5f57204
Kamogelo <4069958@myuwc.ac.za>
2023-02-21 03:55:14 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,112 @@
 #include "main.h"
 /**
- * print_times_table - prints out the n times table, starting with 0
- * @n: times table
+ * print_thousand - print out the digit in the thousand spot
+ * @sum: Value to scan for thousand spot
+ *
+ * Return: Void
+ */
+static void print_thousand(int sum)
+{
+	if (sum / 1000 == 0)
+	{
+		_putchar(32);
+	}
+	else
+	{
+		_putchar('0' + (sum / 1000));
+	}
+}
+
+
+/**
+ * print_hundred - print out the digit in the hundred spot
+ * @sum: Value to scan for hundred spot
+ *
+ * Return: Void
+ */
+static void print_hundred(int sum)
+{
+	if (sum / 100 == 0)
+	{
+		_putchar(32);
+	}
+	else
+	{
+		_putchar('0' + (sum / 100));
+	}
+}
+
+
+/**
+ * print_tenth - print out the digit in the tenth spot
+ * @sum: Value to scan for tenth spot
+ *
+ * Return: Void
+ */
+static void print_tenth(int sum)
+{
+	if (sum / 10 == 0)
+	{
+		_putchar(32);
+	}
+	else
+	{
+		if ((sum / 10) > 9)
+		{
+			_putchar('0' + (sum / 10 % 10));
+		}
+		else
+		{
+			_putchar('0' + (sum / 10));
+		}
+	}
+}
+
+
+/**
+ * print_base10 - print out the digit in the base10 spot
+ * @sum: Value to scan for base10 spot
+ *
+ * Return: Void
+ */
+static void print_base10(int sum)
+{
+	if (sum % 10 == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		_putchar('0' + (sum % 10));
+	}
+}
+
+/**
+ * print_times_table - print out the 9 times table
+ * @n: Valuse use to generate times tables.
+ *
+ * Return: void
  */
 void print_times_table(int n)
 {
-int x, y, z, k, t;
+	int v, h;
 
-	for (x = 0; x <= n; x++)
+	if (n > 15)
 	{
-		for (y = 0; y <= n; y++)
+		return;
+	}
+	for (h = 0 ; h < n + 1 ; h++)
+	{
+		_putchar('0');
+		for (v = 1 ; v < n + 1 ; v++)
 		{
-			z = x * y;
-			if (z == 0 && y == 0)
-				_putchar(z + '0');
-			else if (z > 99)
-			{
-				k = z % 10;
-				t = (z / 10) % 10;
-				z /= 100;
-				_putchar(',');
-				_putchar(' ');
-				_putchar(z + '0');
-				_putchar(t + '0');
-				_putchar(k + '0');
-			}
-			else if (z > 9)
-			{
-				k = z % 10;
-				z /= 10;
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(z + '0');
-				_putchar(k + '0');
+			int sum = v * h;
 
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(z + '0');
-			}
+			_putchar(',');
+			print_thousand(sum);
+			print_hundred(sum);
+			print_tenth(sum);
+			print_base10(sum);
 		}
 		_putchar('\n');
 	}
