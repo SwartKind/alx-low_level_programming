4cf2c793eea0c2551ab89e6731c2dba5648da9bc
Swartkind <105363742+SwartKind@users.noreply.github.com>
2023-02-21 08:01:21 +0200

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * main - print holberton
+ * main - print main function
  * Return: 0 if succesful
  */
 int main(void)
 {
-_putchar(H);
-_putchar(o);
-_putchar(l);
-_putchar(b);
-_putchar(e);
-_putchar(r);
-_putchar(t);
-_putchar(o);
-_putchar(n);
-_putchar(n);
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
 
 return (0);
 }
